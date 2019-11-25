e0231be0b9d9547b69d51156326f7e3bb89c49bf
pineapple79 <fun.goul.109@gmail.com>
2019-11-25 23:15:41 +0600

    Task3-2

---
+++ num2.py
@@ -0,0 +1,9 @@
+
+# input_words = input()
+# word_list = input_words.split(' ')
+# sorted_list = sorted(word_list, key=len)
+# print(' '.join(sorted_list))
+
+a = (sorted(input("Enter the words,please ").split(),key=len))
+a = " ".join(a)
+print(a)
